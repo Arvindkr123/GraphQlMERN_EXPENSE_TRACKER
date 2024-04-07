5dca22ddf7c6a8e84dd9eb43a9ce2bbbb86b942e
Arvind Kumar <thakurarvindkr10@gmail.com>
2024-04-07 13:27:12 +0530

    added ui part

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,24 @@
-export default function App() {
-  return <h1 className="text-6xl font-bold underline">Hello world!</h1>;
+import SignUpPage from "./pages/SignUpPage";
+import HomePage from "./pages/HomePage.jsx";
+import LoginPage from "./pages/LoginPage.jsx";
+import TransactionPage from "./pages/TransactionPage.jsx";
+import NotFound from "./pages/NotFound.jsx";
+import { Routes, Route } from "react-router-dom";
+import Header from "./components/ui/Header.jsx";
+
+function App() {
+  const authUser = true;
+  return (
+    <>
+      {authUser && <Header />}
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/transaction/:id" element={<TransactionPage />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </>
+  );
 }
+export default App;
